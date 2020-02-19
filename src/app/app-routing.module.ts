7ddc3ac40299799fea87d6917fc645752a01ef5d
StarbrandedSentry <hierarchhero@gmail.com>
2020-02-20 01:22:02 +0800

    :rocket: :rocket:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { MenusComponent } from './menus/menus.component';
+import { OrderOnlineComponent } from './order-online/order-online.component';
+import { CateringComponent } from './catering/catering.component';
+import { EventsComponent } from './events/events.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: LandingPageComponent },
+  { path: 'our-story', component: OurStoryComponent },
+  { path: 'menus', component: MenusComponent },
+  { path: 'order-online', component: OrderOnlineComponent },
+  { path: 'catering', component: CateringComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { MenusComponent } from './menus/menus.component';
+import { OrderOnlineComponent } from './order-online/order-online.component';
+import { CateringComponent } from './catering/catering.component';
+import { EventsComponent } from './events/events.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, OurStoryComponent, MenusComponent, OrderOnlineComponent, CateringComponent, EventsComponent, ContactComponent, LandingPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
