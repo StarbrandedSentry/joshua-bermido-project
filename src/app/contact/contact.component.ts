0a3d38d7bf11b660079be899288ac083a8830a55
StarbrandedSentry <hierarchhero@gmail.com>
2020-03-23 19:39:37 +0800

    contact pop-up :corn:

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  name: string;
+  email: string;
+  message: string;
+  success = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onButtonClick() {
+    this.clearText();
+    this.success = true;
   }
 
+  onCloseClick() {
+    this.success = false;
+  }
+
+  clearText(): void {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
 }
